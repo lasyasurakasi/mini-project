79e47b0accf4926190aae8c782d26874893b3cbb
Abhinay Pandey <abhinaypandey02@gmail.com>
2023-04-26 18:57:57 +0530

    professional

--- components/organisms/ProductDesSection.tsx
+++ components/organisms/ProductDesSection.tsx
@@ -25,7 +25,7 @@ const ProductDesSection = ({
           )
         })}
       </div>
-      {similarBoughtCycles.length && (
+      {similarBoughtCycles.length > 0 && (
         <h1 className="mb-7 text-2xl font-semibold text-black">People Also Booked</h1>
       )}
       <div className={'grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'}>
