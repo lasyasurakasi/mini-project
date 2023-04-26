96178b04f19f2b5dbdf5de0e43002444e8acfbcd
Abhinay Pandey <abhinaypandey02@gmail.com>
2023-04-26 17:41:45 +0530

    account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
+import Button from '../components/atoms/Button'
 import Container from '../components/atoms/Container'
 import SEO from '../components/atoms/Seo'
 import Wrapper from '../components/atoms/Wrapper'
 import BookingCard from '../components/molecules/BookingCard'
 import CycleCard from '../components/molecules/CycleCard'
 import Layout from '../components/organisms/Layout'
-import { signInGoogle } from '../firebase/auth'
+import { logOut, signInGoogle } from '../firebase/auth'
 import { getClientBookings, getCycles, getHostBookings, getUserCycles } from '../firebase/firestore'
 import Booking from '../interfaces/Booking'
 import Cycle from '../interfaces/Cycle'
@@ -16,6 +18,7 @@ import { useUser } from './_app'
 
 export default function Account() {
   const { rawUser } = useUser()
+  const router = useRouter()
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [bookingsByMe, setBookingsByMe] = useState<Booking[]>([])
   const [bookingsForMe, setBookingsForMe] = useState<Booking[]>([])
@@ -37,7 +40,7 @@ export default function Account() {
     <Layout>
       <SEO title={rawUser?.displayName || 'Account'} />
       <Wrapper>
-        <Container className={'my-10 flex flex-wrap gap-10 px-5 md:flex-nowrap'}>
+        <Container className={'my-20 flex flex-wrap gap-10 px-5 md:flex-nowrap'}>
           <div className={'mx-auto w-full flex-shrink-0 md:mx-0 md:w-[250px]'}>
             <div className={'w-full  rounded-lg px-10 py-8 shadow-lg'}>
               <div className={'mb-5 w-full overflow-hidden rounded-full border border-p1'}>
@@ -52,12 +55,25 @@ export default function Account() {
               <div className={'mb-4 text-center text-sm italic'}>{rawUser?.email}</div>
 
               <div className={'mt-10 grid grid-cols-2 gap-2 text-lg'}>
-                <b>Earned:</b> <span>₹{moneyEarned}</span>
-                <b>Spent:</b> <span>₹{moneySpent}</span>
+                <b>Earned:</b> <i>₹{moneyEarned}</i>
+                <b>Spent:</b> <i>₹{moneySpent}</i>
               </div>
+              <Button
+                className={'mx-auto mt-6'}
+                onClick={() => logOut().then(() => router.push('/'))}
+                width={100}
+                height={40}
+                id={'logout'}
+              >
+                Sign Out
+              </Button>
             </div>
           </div>
-          <div className={'mx-auto w-full rounded-lg bg-white p-10 shadow-lg md:mx-0 md:w-auto'}>
+          <div
+            className={
+              'mx-auto w-full flex-grow rounded-lg bg-white p-10 shadow-lg md:mx-0 md:w-auto'
+            }
+          >
             <div className={'mb-10'}>
               <div className={'mb-5 text-2xl font-bold'}>My Cycles</div>
               <div className={'flex grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 '}>
@@ -65,6 +81,7 @@ export default function Account() {
                   <CycleCard cycle={cycle} key={cycle.id} />
                 ))}
               </div>
+              {!cycles.length && <div className={'text-center italic'}>No Cycles Added</div>}
             </div>
             <div className={'mb-10'}>
               <div className={'mb-5 text-2xl font-bold'}>My Cycles Booked</div>
